8cc4ccaa5d71866c72b83b770f7e27c7fcd93b58
whelmed <ben@bignoyeslab.com>
2023-10-19 13:45:52 -0500

    cleans up the dunder str method.

--- app.py
+++ app.py
@@ -20,13 +20,10 @@ class TodoModel(Base):
         return f'<Todo {self.id} {self.description} {self.due_date} {self.done}>'
 
     def __str__(self):
-        ''' returns an evenly spaced string representation of the todo item.
-            The done value is represented as [x] for done [ ] for not done.
-            The due date is formatted as YYYY-MM-DD
-        '''
-        done = '[x]' if self.done else '[ ]'
-        date = self.due_date.strftime('%Y-%m-%d')
-        return f'{self.id:3} {self.description:20} {date} {done}'
+        done_status = '[x]' if self.done else '[ ]'
+        formatted_date = self.due_date.strftime('%Y-%m-%d')
+        return f'{self.id:3} {self.description:20} {formatted_date} {done_status}'
+
 
     def __eq__(self, other):
         if not isinstance(other, TodoModel):
